4989297e7ac946f4e43e05a2fa5df85bcd192d26
Gordon Chen <jetaries@gmail.com>
2017-08-18 18:16:14 +0800

    added to-do list.

--- app/components/ToDoApp.jsx
+++ app/components/ToDoApp.jsx
@@ -1,10 +1,32 @@
 var React = require('react');
+var ToDoList = require('ToDoList');
 
 var ToDoApp = React.createClass({
+	getInitialState: function() {
+		return {
+			todos: [
+				{
+					id: 1,
+					text: 'Walk the dog'
+				}, {
+					id: 2,
+					text: 'Clean the yard'
+				}, {
+					id: 3,
+					text: 'Leave mail on porch'
+				}, {
+					id: 4,
+					text: 'Play videogames'
+				}
+			]
+		};
+	},
 	render: function () {
+		var {todos} = this.state;
+
 		return (
 			<div>
-				ToDoApp.jsx
+				<ToDoList todos={todos} />
 			</div>
 		);
 	}
