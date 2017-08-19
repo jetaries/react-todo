ffbd5116c406c660341cfa03ed252ba3f7174f9b
Gordon Chen <jetaries@gmail.com>
2017-08-19 16:32:59 +0800

    wired up search.

--- app/components/ToDoApp.jsx
+++ app/components/ToDoApp.jsx
@@ -43,12 +43,13 @@ var ToDoApp = React.createClass({
 		});
 	},
 	render: function () {
-		var {todos} = this.state;
+		var {todos, showCompleted, searchText} = this.state;
+		var filteredTodos = TodoApi.filterTodos(todos, showCompleted, searchText);
 
 		return (
 			<div>
 				<ToDoSearch onSearch={this.handleSearch} />
-				<ToDoList todos={todos} onToggle={this.handleToggle} />
+				<ToDoList todos={filteredTodos} onToggle={this.handleToggle} />
 				<AddToDo onAddToDo={this.handleAddToDo} />
 			</div>
 		);
--- app/tests/api/TodoApi.test.jsx
+++ app/tests/api/TodoApi.test.jsx
@@ -43,4 +43,39 @@ describe('TodoApi', () => {
 			expect(actualTodos).toEqual(todos);
 		});
 	});
+
+	describe('filterTodos', () => {
+		var todos = [
+			{id: 1, text: "some text here", completed: true},
+			{id: 2, text: "other text here", completed: false},
+			{id: 3, text: "Some text here", completed: true}
+		];
+
+		it('should return all todos if showCompleted is true', () => {
+			var filterTodos = TodoApi.filterTodos(todos, true, "");
+
+			expect(filterTodos.length).toBe(3);
+		});
+
+		it('should return only non-completed todos if showCompleted is false', () => {
+			var filterTodos = TodoApi.filterTodos(todos, false, "");
+
+			expect(filterTodos.length).toBe(1);
+		});
+
+		it('should sort by completed status', () => {
+			var filterTodos = TodoApi.filterTodos(todos, true, "");
+			expect(filterTodos[0].completed).toBe(false);
+		});
+
+		it('should return all todos for empty searchText', () => {
+			var filterTodos = TodoApi.filterTodos(todos, true, "");
+			expect(filterTodos.length).toBe(3);
+		});
+
+		it('should filter todos by searchText', () => {
+			var filterTodos = TodoApi.filterTodos(todos, true, "some");
+			expect(filterTodos.length).toBe(2);
+		});
+	});
 });
