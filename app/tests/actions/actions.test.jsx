1e7f2ffaba243fb936354fc69138ffb95475a49e
Gordon Chen <jetaries@gmail.com>
2017-08-19 23:00:51 +0800

    add actions

---
+++ app/tests/actions/actions.test.jsx
@@ -0,0 +1,42 @@
+var expect = require('expect');
+var actions = require('actions');
+
+describe('actions', () => {
+	it('should generate search text action', () => {
+		var action = {
+			type: "SET_SEARCH_TEXT",
+			searchText: "Some search text"
+		};
+		var res = actions.setSearchText(action.searchText);
+
+		expect(res).toEqual(action);
+	});
+
+	it('should generate add todo action', () => {
+		var action = {
+			type: "ADD_TODO",
+			text: "Some todo text"
+		};
+		var res = actions.addToDo(action.text);
+
+		expect(res).toEqual(action);
+	});
+
+	it('should generate toggle show completed action', () => {
+		var action = {
+			type: "TOGGLE_SHOW_COMPLETED",
+		};
+		var res = actions.toggleShowCompleted();
+
+		expect(res).toEqual(action);
+	});
+
+	it('should generate toggle todo action', () => {
+		var action = {
+			type: "TOGGLE_TODO", id: 3
+		};
+		var res = actions.toggleTodo(action.id);
+
+		expect(res).toEqual(action);
+	});
+});
