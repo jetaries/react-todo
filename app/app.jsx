af08de4f82f573e8e7b08262931f0890c2adcb8c
Gordon Chen <jetaries@gmail.com>
2017-08-21 21:09:57 +0800

    add react router middleware and redirect on/off

--- app/app.jsx
+++ app/app.jsx
@@ -1,12 +1,20 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
-var {Route, Router, IndexRoute, hashHistory} = require('react-router');
+var {hashHistory} = require('react-router');
 var {Provider} = require('react-redux');
 
-import ToDoApp from 'ToDoApp';
-import Login from 'Login';
 var actions = require('actions');
 var store = require('configureStore').configure();
+import firebase from 'app/firebase/';
+import router from 'app/router/';
+
+firebase.auth().onAuthStateChanged((user) => {
+	if (user) {
+		hashHistory.push('/todos');
+	} else {
+		hashHistory.push('/');
+	}
+});
 
 store.dispatch(actions.startAddTodos());
 
@@ -16,14 +24,10 @@ $(document).foundation();
 // App css
 require('style!css!sass!applicationStyles')
 
+
 ReactDOM.render(
   <Provider store={store}>
-  	<Router history={hashHistory}>
-  		<Route path="/" >
-	      <Route path="todos" component={ToDoApp} />
-	      <IndexRoute component={Login} />
-	    </Route>
-  	</Router>
+  	{router}
   </Provider>,
   document.getElementById('app')
 );
